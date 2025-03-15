8ad59171ef24a30693c8f11508f00c46ecde3432
somias <milos.savkovic.somias@gmail.com>
2025-03-15 17:14:35 +0100

    Display ProductsCarousel

--- react-native/app/index.tsx
+++ react-native/app/index.tsx
@@ -1,22 +1,24 @@
 import { RefreshControl, ScrollView, View } from "react-native";
-
 import Skeleton from "@/components/shared/Skeleton";
 import Summary from "@/components/Summary";
 import RecentTransaction from "@/components/RecentTransactions";
 import { SafeAreaView } from "react-native-safe-area-context";
+import ProductsCarousel from "@/components/ProductsCarousel";
 
 export default function HomeRoute() {
   return (
     <SafeAreaView className="flex-1">
       <ScrollView
+        className="flex-1"
         refreshControl={
           <RefreshControl refreshing={false} onRefresh={() => {}} />
         }
-        contentContainerClassName="p-4 flex-1 gap-12 dark:bg-gray-900 text-white light:bg-gray-100 text-black"
+        contentContainerClassName="p-4 flex-1 gap-6 dark:bg-gray-900 text-white light:bg-gray-100 text-black"
       >
-        <Skeleton className="h-64" />
-
-        <View className="gap-6 pt-5 flex-1">
+        <View className="h-80">
+          <ProductsCarousel />
+        </View>
+        <View className="gap-6 flex-1 mt-10">
           <Summary />
           <RecentTransaction />
         </View>
---
+++ react-native/components/ProductsCarousel.tsx
@@ -0,0 +1,29 @@
+import { FlatList, Image, Dimensions, View } from "react-native";
+import { useProducts } from "@/hooks/useProducts";
+
+export default function ProductsCarousel() {
+  const { data } = useProducts();
+  const { width } = Dimensions.get("window");
+
+  return (
+    <FlatList
+      horizontal
+      data={data}
+      pagingEnabled={true}
+      snapToInterval={width}
+      snapToAlignment="start"
+      showsHorizontalScrollIndicator={false}
+      decelerationRate="fast"
+      keyExtractor={(product) => product.id}
+      renderItem={({ item }) => (
+        <View className="flex" style={{ width }}>
+          <Image
+            source={{ uri: item.image }}
+            className="h-80 object-cover"
+            style={{ width }}
+          />
+        </View>
+      )}
+    />
+  );
+}
